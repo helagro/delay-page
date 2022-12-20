d2037cb3bedcccf10ec733231da8b19a0f30e895
I <helagro@outlook.com>
2022-12-20 14:04:44 +0100

    init

---
+++ script.js
@@ -0,0 +1,33 @@
+const DEFAULT_DELAY = 5
+
+function main(){
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+
+    if( !hasParams(urlParams) ){
+        alert("Missing destination")
+        return
+    }
+
+    const openDestinationRunnable = () => {
+        openDestination(urlParams)
+    }
+    delay(urlParams, openDestinationRunnable)
+}
+
+function hasParams(urlParams){
+    return urlParams.has("d")
+}
+
+function delay(urlParams, callback){
+    const delayDuration = urlParams.has("delay") ? urlParams.get("delay") : DEFAULT_DELAY
+    setTimeout(callback, delayDuration)
+}
+
+function openDestination(urlParams){
+    const destination = urlParams.get("d")
+    window.open(destination, "_self", false)
+}
+
+
+main()
